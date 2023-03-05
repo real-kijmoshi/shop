979fb361b48edee64294f706bdf484ad30c5fe65
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-03-05 18:05:08 +0100

    quick fix v4

--- pages/components/product.jsx
+++ pages/components/product.jsx
@@ -5,7 +5,7 @@ import styles from "../../styles/Product.module.css";
 export default function Product(props) {
   return (
     <div className={styles.product}>
-      <img src={props.data?.image || ""} />
+      <img src={props.data?.image || ""} style={{width: 100, height: "auto"}} />
       <h1>{props.data?.product || ""}</h1>
     </div>
   );
