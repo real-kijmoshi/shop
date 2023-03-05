c111a6396d783d639ccd0963ceec09937a388ac0
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-03-05 17:51:47 +0100

    quick fix v2

--- pages/components/product.jsx
+++ pages/components/product.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import styles from "../../styles/Product.module.css"
 
 export default function Product(props) {
   return (
     <div className={styles.product}>
-      <img src={props.data.image} style={{width: 100, height: "auto"}}/>
+      <img src={props.data?.image || ""} />
       <h1>{props.data.product}</h1>
     </div>
   );
