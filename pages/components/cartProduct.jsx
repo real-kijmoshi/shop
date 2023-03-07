121010acf3a22c75495199588552d6a460f2f796
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-03-07 08:51:11 +0100

    cart update fix

--- pages/components/cartProduct.jsx
+++ pages/components/cartProduct.jsx
@@ -1,10 +1,10 @@
 import ImageSlider from "./imageSlider"
 
 export default function cartProduct(props) {
-  const { product: name, images, price, quantity } = props.data;
+  const { product: name, images, price, quantity, id } = props.data;
 
   return (
-    <div key={props.data.id}>
+    <div key={id}>
       <h1>{name}</h1>
       <ImageSlider images={images} width={25} />
       <h2>{price}$  quanity: {quantity}</h2>
