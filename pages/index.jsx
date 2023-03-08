93f5778e07ce4a597a3aa179a40bb5aa6890e87a
kijmoshi <94071547+real-kijmoshi@users.noreply.github.com>
2023-03-08 08:36:13 +0100

    cart update

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Product from "./components/product.jsx";
 import styles from "../styles/Home.module.css";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
+import * as Icon from "react-bootstrap-icons"
 
 export default function Home() {
   const [products, setProducts] = useState(data);
@@ -37,6 +38,14 @@ export default function Home() {
         ) : (
           <h1> Nothing found </h1>
         )}
+      </div>
+
+      <div id="cart" style={{width: "100%", position: "fixed", bottom: 0, display: "flex", justifyContent: "right", paddingRight: 20}}>
+        <div style={{backgroundColor: "#120aff", width: 70, height: 70, borderRadius: 50, display: "flex", flexDirection: "column", justifyContent: "center", marginBottom: 10}}>
+          <div style={{textAlign: "center"}}>
+            <Icon.CartFill size={30} color="white" />
+          </div>
+        </div>
       </div>
     </>
   );
